829c028eb3a9fd96fa6e19d95fef83dee062882c
Akinola John <34023991+officialhord@users.noreply.github.com>
2021-07-24 13:22:57 +0100

    Project startup

---
+++ src/main/java/com/vida/VidaApplication.java
@@ -0,0 +1,13 @@
+package com.vida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vida/VidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
