6376938989747db338ef707d4d6f0f283f313fd8
Akinola John <34023991+officialhord@users.noreply.github.com>
2021-07-31 17:47:30 +0100

    Updating error handling

--- src/main/java/com/vida/Messenger/ErrorMessage.java
+++ src/main/java/com/vida/Messenger/ErrorMessage.java
@@ -1,19 +1,21 @@
 package com.vida.Messenger;
 
-import com.vida.Vehicle;
-import org.hibernate.service.spi.ServiceException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.logging.LogLevel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 public class ErrorMessage {
 
     private String message = null;
 
-    public void invalidDate(String date) {
+    @GetMapping("/vehicle")
+    public ResponseEntity<String> invalidDate(@RequestParam String modelYear) {
 
-        //This should return an Invalid year entered message as the webservice response
-        message = "Invalid year entered... " + date;
-
+        message = "Invalid year entered... " + modelYear;
+        return new ResponseEntity(message, HttpStatus.NOT_ACCEPTABLE);
     }
+
+
 }
